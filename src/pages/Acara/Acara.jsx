e256f441c10966bde0df62889cd82f41a864eb8f
muamaralf <muamar.alf@gmail.com>
2024-08-30 10:12:35 +0700

    edit acara

--- src/pages/Acara/Acara.jsx
+++ src/pages/Acara/Acara.jsx
@@ -8,38 +8,34 @@ import dataAcara from "../../data/Acara/acara";
 import instance from "../../services/axios/instance";
 
 const Acara = () => {
-<<<<<<< HEAD
   const [events, setEvents] = useState;
-=======
   const [event, setEvent] = useState([]);
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetchData()
-  }, [search])
+    fetchData();
+  }, [search]);
 
   const fetchData = async () => {
     try {
-      const {data} = await instance.get('/events', {
+      const { data } = await instance.get("/events", {
         params: {
           q: search,
-        }
-      })
+        },
+      });
 
-      setEvent(data.data)
+      setEvent(data.data);
     } catch (e) {
-      console.log(e)
-      alert(e?.response?.data?.message || 'Error')
+      console.log(e);
+      alert(e?.response?.data?.message || "Error");
     }
   };
-
->>>>>>> 2138688bf1743f465b4ea269daed5aeeac6d3fed
   return (
     <div className="container">
       <div className="flex flex-col gap-5">
         <div className="flex flex-col gap-2 bg-[#50B478] p-6">
           <BackButton text="Acara" clss="text-white" clssArrw="fill-white" />
-          <SearchBar onChange={value => setSearch(value)} />
+          <SearchBar onChange={(value) => setSearch(value)} />
         </div>
 
         <div className="flex flex-col gap-4 mt-4 px-6 pb-6">
